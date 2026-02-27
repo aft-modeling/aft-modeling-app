a3a409db56578d54b2860976ab007a59be9c9885
aft-modeling <jay@aftmodeling.com>
2026-02-26 18:39:23 -0800

    Skip TS and ESLint errors during build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
+    typescript: {
+          ignoreBuildErrors: true,
+    },
+    eslint: {
+          ignoreDuringBuilds: true,
+    },
+    experimental: {
+          serverActions: true,
+    },
+    images: {
+          domains: ['lh3.googleusercontent.com'],
+    },
 }
 
 module.exports = nextConfig
